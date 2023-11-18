6d6fad4ee4ccee7c12110078e1ff8d6309122085
Flychee <1229853676@qq.com>
2023-11-18 17:12:39 +0800

    登录函数

--- Neuvillette/data/views.py
+++ Neuvillette/data/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+import json
+from django.http import JsonResponse
+from models import *
+
 
 # Create your views here.
+# 登录函数
+def login(request):
+    if request.method == 'POST':
+        post = request.POST
+        result = {'login_id': post['login_id'], 'authority_level': -1}
+        check_data = User.objects.filter(login_id=post['login_id']).filter(login_password=post['login_password'])
+        if check_data:
+            result['authority_level'] = check_data[0].authority_level
+        return JsonResponse(json.dumps(result), safe=False)
