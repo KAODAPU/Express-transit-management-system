5f6aa27379eb59fd919303900db1c83e839ca282
Flychee <1229853676@qq.com>
2023-11-20 15:15:11 +0800

    新增快递线路表

--- Neuvillette/data/models.py
+++ Neuvillette/data/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 # Create your models here.
 
-# 物品类
+# 物品表
 class Item(models.Model):
     item_id = models.BigIntegerField()  # 物品id
     client_id = models.BigIntegerField()  # 寄件人id
@@ -19,13 +19,13 @@ class Item(models.Model):
     is_receive = models.BooleanField(default=False)  # 是否收货
 
 
-# 快递站地址类
+# 快递站地址表
 class Address(models.Model):
     Address_id = models.BigIntegerField()  # 地址id
     name = models.CharField(max_length=100)  # 地址名
 
 
-# 快递中转查询
+# 快递中转查询表
 class Item2Address(models.Model):
     item_id = models.BigIntegerField()  # 物品id
     Address_id = models.BigIntegerField()  # 地址id
@@ -34,7 +34,19 @@ class Item2Address(models.Model):
     date = models.DateTimeField(auto_now_add=True)  # 录入时间
 
 
-# 用户类
+# 快递线路表
+class Line(models.Model):
+    line_id = models.AutoField(primary_key=True)  # 线路id
+
+
+# 快递线路从表，存储线路上每个地址
+class LineAddress(models.Model):
+    address_id = models.BigIntegerField()  # 地址id
+    transfer_id = models.AutoField(primary_key=True)  # 中转id(注：用于查询在线路中第几站)
+    line = models.ForeignKey('data.Line', on_delete=models.CASCADE)  # 作为快递线路表从表
+
+
+# 用户表
 class User(models.Model):
     login_id = models.BigIntegerField()  # 账户id
     login_password = models.TextField()  # 密码
