eb1109cd7c51f18d35836ec45b71decb33c1da1c
MZiShi <1715902202@qq.com>
2023-11-24 22:13:34 +0800

    优化logo显示

--- management/src/layout/components/Sidebar/Logo.vue
+++ management/src/layout/components/Sidebar/Logo.vue
@@ -1,13 +1,8 @@
 <template>
   <div class="sidebar-logo-container" :class="{'collapse':collapse}">
     <transition name="sidebarLogoFade">
-      <router-link v-if="collapse" key="collapse" class="sidebar-logo-link" to="/">
-        <img v-if="logo" :src="logo" class="sidebar-logo">
-        <h1 v-else class="sidebar-title">{{ title }} </h1>
-      </router-link>
-      <router-link v-else key="expand" class="sidebar-logo-link" to="/">
-        <img v-if="logo" :src="logo" class="sidebar-logo">
-        <h1 class="sidebar-title">{{ title }} </h1>
+      <router-link key="collapse" class="sidebar-logo-link" to="/">
+        <img src="@/assets/common/logo1.png" class="sidebar-logo">
       </router-link>
     </transition>
   </div>
@@ -24,8 +19,8 @@ export default {
   },
   data() {
     return {
-      title: 'Vue Admin Template',
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      title: '快递物流管理系统',
+      logo: 'src\assets\common\logo1.png'
     }
   }
 }
