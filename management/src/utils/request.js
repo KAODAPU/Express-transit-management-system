49c4466fd6b7e0b644302fe200e657b61f6c6041
MZiShi <1715902202@qq.com>
2023-12-03 18:49:28 +0800

    2023/12/3

--- management/src/utils/request.js
+++ management/src/utils/request.js
@@ -1,41 +1,24 @@
 import axios from 'axios'
-// import store from '@/store'
-// import { Message } from 'element-ui'
-// import router from '@/router'
+import { Message } from 'element-ui'
 const service = axios.create({
   baseURL: 'http://192.168.3.22:8000', // 基础地址
   timeout: 10000
-}) // 创建一个新的axios实例
-// 成功1 失败2
-// service.interceptors.request.use((config) => {
-//   // 注入token
-//   return config
-// }, (error) => {
-//   // 失败执行promise
-//   return Promise.reject(error)
-// })
+})
+service.interceptors.request.use((config) => {
+  // 注入token
+  return config
+}, (error) => {
+  // 失败执行promise
+  return Promise.reject(error)
+})
 
-// 响应拦截器
-// service.interceptors.response.use((response) => {
-//   // axios默认包裹了data
-//   // const { data, message, success } = response.data
-//   // if (success) {
-//   //   return data
-//   // } else {
-//   //   Message({ type: 'error', message })
-//   //   return Promise.reject(new Error(message))
-//   // }
-// }, async(error) => {
-//   // if (error.response.status === 401) {
-//   //   Message({ type: 'warning', message: 'token超时了' })
-//   //   // 说明token超时了
-//   //   await store.dispatch('user/logout') // 调用action 退出登录
-//   //   //  主动跳到登录页
-//   //   router.push('/login') // 跳转到登录页
-//   //   return Promise.reject(error)
-//   // }
-//   // // error.message
-//   // Message({ type: 'error', message: error.message })
-//   // return Promise.reject(error)
-// })
+service.interceptors.response.use((response) => {
+  const { data, success } = JSON.parse(response.data)
+  if (success) {
+    return data
+  } else {
+    Message({ type: 'error' })
+    return data
+  }
+})
 export default service
